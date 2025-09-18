51ddcad6bbec6e4c0d6d9208add42a8454e5d795
kimoban <isaiahkimoban87@gmail.com>
2025-09-18 22:20:59 +0000

    feat: add PWA support (manifest, service worker, offline caching)

---
+++ frontend/public/service-worker.js
@@ -0,0 +1,21 @@
+// Simple service worker for Expo web PWA
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open('v1').then(cache => {
+      return cache.match(event.request).then(response => {
+        return response || fetch(event.request).then(networkResponse => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      });
+    })
+  );
+});
