4832533d142eaa685a15b0c3d369d97d02491227
kimoban <isaiahkimoban87@gmail.com>
2025-09-18 00:24:09 +0000

    fix: remove unmatched bracket and erroneous line from urls.py

--- backend/ecommerce/urls.py
+++ backend/ecommerce/urls.py
@@ -40,5 +40,3 @@ urlpatterns = [
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('', include('ecommerce.views_and_root_urls')),
 ]
-urlpatterns += root_urlpatterns
-]
