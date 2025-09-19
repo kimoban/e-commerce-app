380eea8b020a6670fbc1be6605cf4647d60d47c5
kimoban <isaiahkimoban87@gmail.com>
2025-09-19 21:53:08 +0000

    fix: update babel config for vercel deployment

--- frontend/babel.config.cjs
+++ frontend/babel.config.cjs
@@ -1,7 +1,21 @@
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: ['nativewind/babel'],
-  };
+module.exports = {
+  presets: ['babel-preset-expo'],
+  plugins: [
+    'nativewind/babel',
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@store': './src/store',
+          '@navigation': './src/navigation',
+          '@utils': './src/utils',
+          '@constants': './src/constants',
+          '@hooks': './src/hooks',
+          '@assets': './src/assets',
+        },
+      },
+    ],
+  ],
 };
