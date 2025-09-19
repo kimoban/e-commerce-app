3cf1072391918098c1f4441bc1149d1ae0e81d9b
kimoban <isaiahkimoban87@gmail.com>
2025-09-19 22:01:03 +0000

    fix: revert babel config to function export for vercel

--- frontend/babel.config.cjs
+++ frontend/babel.config.cjs
@@ -1,21 +1,24 @@
-module.exports = {
-  presets: ['babel-preset-expo'],
-  plugins: [
-    'nativewind/babel',
-    [
-      'module-resolver',
-      {
-        alias: {
-          '@components': './src/components',
-          '@screens': './src/screens',
-          '@store': './src/store',
-          '@navigation': './src/navigation',
-          '@utils': './src/utils',
-          '@constants': './src/constants',
-          '@hooks': './src/hooks',
-          '@assets': './src/assets',
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      'nativewind/babel',
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@components': './src/components',
+            '@screens': './src/screens',
+            '@store': './src/store',
+            '@navigation': './src/navigation',
+            '@utils': './src/utils',
+            '@constants': './src/constants',
+            '@hooks': './src/hooks',
+            '@assets': './src/assets',
+          },
         },
-      },
+      ],
     ],
-  ],
+  };
 };
