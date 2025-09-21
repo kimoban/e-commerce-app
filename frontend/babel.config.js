f51a514b33266489206f46a71ac672b433a99987
kimoban <isaiahkimoban87@gmail.com>
2025-09-21 10:47:04 +0000

    Fix babel.config.js for Expo and NativeWind compatibility

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,26 +1,29 @@
-module.exports = {
-  presets: ['babel-preset-expo'],
-  plugins: [
-    'nativewind/babel',
-    [
-      'module-resolver',
-      {
-        root: ['./src'],
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-        alias: {
-          '@assets': './src/assets',
-          '@components': './src/components',
-          '@screens': './src/screens',
-          '@navigation': './src/navigation',
-          '@store': './src/store',
-          '@services': './src/services',
-          '@hooks': './src/hooks',
-          '@utils': './src/utils',
-          '@constants': './src/constants',
-          '@types': './src/types',
-          '@config': './src/config',
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      'nativewind/babel',
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+          alias: {
+            '@assets': './src/assets',
+            '@components': './src/components',
+            '@screens': './src/screens',
+            '@navigation': './src/navigation',
+            '@store': './src/store',
+            '@services': './src/services',
+            '@hooks': './src/hooks',
+            '@utils': './src/utils',
+            '@constants': './src/constants',
+            '@types': './src/types',
+            '@config': './src/config',
+          },
         },
-      },
+      ],
     ],
-  ],
+  };
 };
